31ebaf3e92ed79c5b5a5016f80416926c8f72295
Daniel Ko <dankomong@gmail.com>
2019-03-07 20:51:05 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_arr = esperanto.split('')
+  
+  arr.sort_by do |word|
+    word.split('').map do |letter|
+      esperanto_arr.index(letter)
+    end
+  end
 end
